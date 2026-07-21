d18345b498b06999e66a22f485cd2ae7bc973fc3
Asko Soukka <asko.soukka@iki.fi>
2026-07-21 21:50:22 +0300

    Fix to mount ~/.config/git/config for antigravity

--- tweaks/antigravity.nix
+++ tweaks/antigravity.nix
@@ -10,6 +10,7 @@
     lib.optionalAttrs (lib.elem "antigravity" cfg.tweaks) {
       mounts = [
         "source=\${localEnv:HOME}/.gitconfig,target=/home/vscode/.gitconfig,type=bind,readonly"
+        "source=\${localEnv:HOME}/.config/git,target=/home/vscode/.config/git,type=bind,readonly"
       ];
       postStartCommand = "(git config --get user.signingkey | xargs -I {} gpg --keyserver keyserver.ubuntu.com --recv-keys {} || true)";
     };
